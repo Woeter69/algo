760c4b651a75a27db18a8213097220257e4ec80c
Woeter <skippedlead@gmail.com>
2025-09-06 02:50:43 +0530

    v0.4 Email Verification working

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -9,15 +9,25 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     registration_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    verified BOOLEAN DEFAULT FALSE,   -- NEW: track email verification
 
     -- Profile info (can be NULL initially)
     dob DATE,
     graduation_year INT,
     university_name VARCHAR(255),
     department VARCHAR(255),
+    college VARCHAR(255),
     interests TEXT,
     preferred_contact ENUM('email','phone','linkedin'),
     linkedin_profile VARCHAR(255),
     current_city VARCHAR(255),
     phone_number VARCHAR(20)
+);
+
+-- Table to store email verification tokens
+CREATE TABLE IF NOT EXISTS verification_tokens (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    token VARCHAR(255) UNIQUE NOT NULL,
+    expiry DATETIME NOT NULL
 );
---
+++ app/src/validators.py
@@ -0,0 +1,22 @@
+import os
+import smtplib 
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+
+load_dotenv()
+
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASS = os.getenv("EMAIL_PASS")
+print(EMAIL_USER, EMAIL_PASS)
+def send_verification_email(to_email,link):
+    msg = MIMEText(f"Click this link to verify your account:\n\n{link}")
+    msg["Subject"] = "Verify your Alumni Platform account"
+    msg["From"] = EMAIL_USER
+    msg["To"] = to_email
+
+
+    with smtplib.SMTP("smtp.gmail.com",587) as server:
+        server.starttls()
+        server.login(EMAIL_USER,EMAIL_PASS)
+        server.send_message(msg)
+
