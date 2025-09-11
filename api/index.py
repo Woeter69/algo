5b989062ff32c4dce80be1af521728a9cc12eeb1
Woeter <skippedlead@gmail.com>
2025-09-11 13:56:56 +0530

    fixex hambuger menu

--- api/index.py
+++ api/index.py
@@ -1,10 +1,2 @@
-import sys, os
-# add the folder containing app.py to sys.path
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'app', 'src'))
-
-from app import app  # import Flask app instance from app.py
-
-from vercel_python_wsgi import serverless_wsgi
-
-def handler(event, context):
-    return serverless_wsgi.handle_request(app, event, context)
+# api/index.py
+from app.src.app import app  # <- this points to root/app/src/app.py
