a66c55249b469d58dc027f5b466c9188067a034d
Woeter <skippedlead@gmail.com>
2025-09-12 20:42:15 +0530

    changed something idk

--- app/src/connection.py
+++ app/src/connection.py
@@ -3,7 +3,7 @@ import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
         host = "localhost",
-        user = "root",
-        passwd = "nandini",
+        user = "alumni_user",
+        passwd = "root",
         database = "alumni_platform"
     )
