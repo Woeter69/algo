bb481dc9bcef74f8e3008bc3486c3e2d30c61201
luneseline <nandinidevatwal2@gmail.com>
2025-09-12 14:39:46 +0530

    Fixed schema.sql

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -92,23 +92,13 @@ CREATE TABLE IF NOT EXISTS education_details(
 
 );
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> 5bef8401dd0ae414d2dadb8f40ad580d1d8a106d
---                                          ovesh start
---contacts table for user feedback
-=======
-
--- contacts table
->>>>>>> 9fdc72160a1033c94540fc3196e601cfdf1c0b87
 CREATE TABLE IF NOT EXISTS contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
     phone VARCHAR(20),
-    subject VARCHAR(255)
+    subject VARCHAR(255),
     message TEXT NOT NULL,
     submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )ENGINE=InnoDB;
--- app/src/connection.py
+++ app/src/connection.py
@@ -4,6 +4,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host = "localhost",
         user = "root",
-        passwd = "root",
+        passwd = "nandini",
         database = "alumni_platform"
     )
