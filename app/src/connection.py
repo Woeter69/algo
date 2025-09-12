59e1e1fe052f6418dea2064532c8931e4c32ae52
Pranjul Gupta <pranjul.here@gmail.com>
2025-09-12 13:48:57 +0530

    Merge branch 'main' of https://github.com/Woeter69/sih-project-2025 into frontend

--- app/src/connection.py
+++ app/src/connection.py
@@ -3,7 +3,7 @@ import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
         host = "localhost",
-        user = "alumni_user",
+        user = "root",
         passwd = "root",
         database = "alumni_platform"
     )
