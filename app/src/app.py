154f51989337e58a1ac873f788afbd9dc1280e9c
Woeter <skippedlead@gmail.com>
2025-09-15 22:32:53 +0530

    Fixed flash issues

--- app/src/app.py
+++ app/src/app.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from flask import Flask,render_template,request, session, redirect, url_for
+from flask import Flask,render_template,request, session, redirect, url_for,flash
 from . import connection 
 import secrets, datetime
 from flask_bcrypt import Bcrypt
